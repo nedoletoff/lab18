56e486da0f15cb5b89b85534ea1c7543c53c41c7
nedoletoff <nedoletoff@yandex.ru>
2021-10-19 16:13:42 +0300

    deleted garbage

--- main.cpp
+++ main.cpp
@@ -4,10 +4,7 @@ int main()
 {
 	LruCashe l(5);
 	std::cout << "l - " << l << std::endl;
-<<<<<<< HEAD
 	std::cout << "l size - " << l.size() << std::endl;
-=======
->>>>>>> ab495ab268e5fcb54c6ca22bf8b4b2a47a7061c5
 	l.find_and_add("rer");
 	l.find_and_add("r");
 	l.find_and_add("ab");
@@ -16,7 +13,6 @@ int main()
 	std::cout << "l size - " << l.size() << std::endl;
 	std::cout << "l - " << l << std::endl;
 	l.find_and_add("oo");
-<<<<<<< HEAD
 	l.find_and_add("adr");
 	std::cout << "l size - " << l.size() << std::endl;
 	std::cout << "l - " << l << std::endl;
@@ -24,14 +20,6 @@ int main()
 	std::cout << "l size - " << l.size() << std::endl;
 	std::cout << "l - " << l << std::endl;
 	std::cout << "is AAA in list - " << l.find_and_add("AAA") << std::endl;
-=======
-	std::cout << "l size - " << l.size() << std::endl;
-	std::cout << "l - " << l << std::endl;
-	std::cout << l.find_and_add("oo") << std::endl;
-	std::cout << "l size - " << l.size() << std::endl;
-	std::cout << "l - " << l << std::endl;
-	std::cout << l.find_and_add("AAA") << std::endl;
->>>>>>> ab495ab268e5fcb54c6ca22bf8b4b2a47a7061c5
 	std::cout << "l size - " << l.size() << std::endl;
 	std::cout << "l - " << l << std::endl;
 
